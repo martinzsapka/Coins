63d395388cf26018ef704eff64dd79afedc583c4
Martin Zsapka <martin.zsapka@gmail.com>
2025-06-08 14:58:40 +0200

    Project init

---
+++ Coins/CoinsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinsApp.swift
+//  Coins
+//
+//  Created by Support Aximmetry on 08/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct CoinsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
