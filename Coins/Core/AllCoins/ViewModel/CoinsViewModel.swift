74b09b05b6f3e9453946252f0ad61dd76cd3b841
Martin Zsapka <martin.zsapka@gmail.com>
2025-06-10 10:05:41 +0200

    Dependency Injection

--- Coins/Core/AllCoins/ViewModel/CoinsViewModel.swift
+++ Coins/Core/AllCoins/ViewModel/CoinsViewModel.swift
@@ -11,9 +11,11 @@ class CoinsViewModel: ObservableObject {
     @Published var coins = [Coin]()
     @Published var errorMessage: String?
     
-    private let service = CoinDataService()
+    private let service: CoinDataService
     
-    init() {
+    init(service: CoinDataService) {
+        self.service = service
+        
         Task {
             await fetchCoins()
         }
