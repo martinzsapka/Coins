6c4d344568badd24ab2043a25f1fb826fbbece70
Martin Zsapka <martin.zsapka@gmail.com>
2025-06-11 10:10:59 +0200

    Add CoinListView

--- Coins/ContentView.swift
+++ Coins/ContentView.swift
@@ -9,42 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     private let service: CoinDataServiceProtocol
-    @StateObject var viewModel: CoinListViewModel
     
     init(service: CoinDataServiceProtocol) {
         self.service = service
-        self._viewModel = StateObject(wrappedValue: CoinListViewModel(service: service))
     }
     
     var body: some View {
-        NavigationStack {
-            List {
-                ForEach(viewModel.coins) { coin in
-                    NavigationLink(value: coin) {
-                        HStack(spacing: 12) {
-                            Text("\(coin.marketCapRank)")
-                                .foregroundStyle(.gray)
-                            
-                            VStack(alignment: .leading, spacing: 4) {
-                                Text(coin.name)
-                                    .fontWeight(.semibold)
-                                
-                                Text(coin.symbol.uppercased())
-                            }
-                        }
-                        .font(.footnote)
-                    }
-                }
-            }
-            .navigationDestination(for: CoinListItem.self, destination: { coin in
-                CoinDetailsView(coin: coin, service: service)
-            })
-            .overlay {
-                if let error = viewModel.errorMessage {
-                    Text(error)
-                }
-            }
-        }
+        CoinListView(service: service)
     }
 }
 
---
+++ Coins/Core/CoinList/View/CoinListView.swift
@@ -0,0 +1,53 @@
+//
+//  CoinListView.swift
+//  Coins
+//
+//  Created by Support Aximmetry on 10/06/2025.
+//
+
+import SwiftUI
+
+struct CoinListView: View {
+    private let service: CoinDataServiceProtocol
+    @StateObject var viewModel: CoinListViewModel
+    
+    init(service: CoinDataServiceProtocol) {
+        self.service = service
+        self._viewModel = StateObject(wrappedValue: CoinListViewModel(service: service))
+    }
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(viewModel.coins) { coin in
+                    NavigationLink(value: coin) {
+                        HStack(spacing: 12) {
+                            Text("\(coin.marketCapRank)")
+                                .foregroundStyle(.gray)
+                            
+                            VStack(alignment: .leading, spacing: 4) {
+                                Text(coin.name)
+                                    .fontWeight(.semibold)
+                                
+                                Text(coin.symbol.uppercased())
+                            }
+                        }
+                        .font(.footnote)
+                    }
+                }
+            }
+            .navigationDestination(for: CoinListItem.self, destination: { coin in
+                CoinDetailsView(coin: coin, service: service)
+            })
+            .overlay {
+                if let error = viewModel.errorMessage {
+                    Text(error)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    CoinListView(service: MockCoinDataService())
+}
