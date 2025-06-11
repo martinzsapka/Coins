dbb9a34d2af91b3fd218555da809c2f441facfb6
Martin Zsapka <martin.zsapka@gmail.com>
2025-06-11 11:46:28 +0200

    Coins data Unit Tests

---
+++ CoinsTests/CoinsTests.swift
@@ -0,0 +1,22 @@
+//
+//  CoinsTests.swift
+//  CoinsTests
+//
+//  Created by Support Aximmetry on 11/06/2025.
+//
+
+import XCTest
+@testable import Coins
+
+final class CoinsTests: XCTestCase {
+
+    func test_DecodeCoinsIntoArray() throws {
+        do {
+            let coins = try JSONDecoder().decode([CoinListItem].self, from: testCoinsData)
+            XCTAssertEqual(coins.count, 20)
+            XCTAssertEqual(coins, coins.sorted(by: { $0.marketCapRank < $1.marketCapRank }))
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
+    }
+}
